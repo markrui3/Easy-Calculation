cdf63dc8df995822f181b3c8f1ac8808134674bb
toddlt <tju.liutong@gmail.com>
2015-10-15 23:56:26 +0800

    导入ThinkPHP和amaze

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,53 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
+    <title>四则运算网站</title>
+    <!-- Bootstrap -->
+    <script type="text/javascript" src="/caculation/Public/assets/js/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="/caculation/Public/assets/css/amazeui.min.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="/caculation/Public/assets/css/amazeui.flat.min.css" /> -->
+    <script type="text/javascript" src="/caculation/Public/assets/js/amazeui.min.js"></script>
+    
+    <style type="text/css">
+	    header {
+	        text-align: center;
+	    }
+    </style>
+
+    <script>
+        baseUrl = '/caculation';
+    </script>
+</head>
+<header class="am-g my-head">
+    <div class="am-u-sm-12 am-article">
+    	<hr>
+        <h1 class="am-article-title">小学生的四则运算</h1>
+        <p class="am-article-meta">崔晨晖，刘桐，谢杭伦，赵锐</p>
+    </div>
+</header>
+<hr class="am-article-divider">
+<div class="am-g am-g-fixed">
+    <div class="am-u-md-9 am-u-md-push-3">
+        <div class="am-g">
+            <div class="am-u-sm-11 am-u-sm-centered">
+                <div class="am-cf am-article">
+                    －－－no content－－－
+                </div>
+                <hr>
+            </div>
+        </div>
+    </div>
+	<div class="am-u-md-3 am-u-md-pull-9">
+	    <ul class="am-nav">
+	        <li><a href="#">加法运算</a></li>
+	        <li><a href="#">减法运算</a></li>
+	        <li><a href="#">乘法运算</a></li>
+	        <li><a href="#">除法运算</a></li>
+	    </ul>
+	</div>
+    <a href="#sidebar" class="am-btn am-btn-sm am-btn-success am-icon-bars am-show-sm-only my-button" data-am-offcanvas=""><span class="am-sr-only">侧栏导航</span></a>
+</div>
