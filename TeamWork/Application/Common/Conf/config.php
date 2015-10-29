4e84e0be223a798bcf7a3b60422a425eff61243f
崔晨晖 <catoldcui@gmail.com>
2015-10-29 14:34:06 +0800

    数据库配置，bower配置前端框架

--- TeamWork/Application/Common/Conf/config.php
+++ TeamWork/Application/Common/Conf/config.php
@@ -1,24 +1,21 @@
 <?php
 return array(
-    //'配置项'=>'配置值'
-    'DB_TYPE'   => '', // 数据库类型
-    'DB_HOST'   => '', // 服务器地址
-    'DB_NAME'   => '', // 数据库名
-    'DB_USER'   => '', // 用户名
-    'DB_PWD'    => '', // 密码
+	//'配置项'=>'配置值'
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => '182.92.168.7', // 服务器地址
+    'DB_NAME'   => 'test', // 数据库名
+    'DB_USER'   => 'tju2015', // 用户名
+    'DB_PWD'    => 'tju2015', // 密码
     'DB_PORT'   => 3306, // 端口
     'DB_PREFIX' => '', // 数据库表前缀
     'DB_CHARSET'=> 'utf8', // 字符集
 
     //'配置项'=>'配置值'
-    'DEFAULT_MODULE' => 'Home',
+    'DEFAULT_GROUP' => 'Home',
     'DEFAULT_CHARSET' => 'utf-8', // 默认输出编码
-    'URL_MODEL'          => '2', //URL模式
     'SESSION_AUTO_START' => true,
-    'DATA_CACHE_KEY'=>'think'
-   /* 'TMPL_TEMPLATE_SUFFIX '=>'.tpl'*/
-    //调试
-/*    'TMPL_L_DELIM'=>'<{',
-    'TMPL_R_DELIM'=>'}>',*/
+    'URL_MODEL' => 2,
+    'LOG_RECORD' => true, // 开启日志记录
+    'LOG_LEVEL'  =>'INFO',
+    'DEFAULT_TIMEZONE' => 'Asia/ShangHai' , // 默认时区
 );
-?>
