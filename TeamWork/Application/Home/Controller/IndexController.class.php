116b38d5a4ae5d62fcbcbef61a838beb5e65707c
toddlt <tju.liutong@gmail.com>
2015-10-30 20:26:15 +0800

    写了教师的导航页和登录页面

--- TeamWork/Application/Home/Controller/IndexController.class.php
+++ TeamWork/Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,54 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-       $this->display('index');
+        $this->display('index');
     }
+    public function test(){
+        $a1 = $this->generateDivide(10);
+        $a2 = $this->generateMultiply(10);
+
+        $result = array_merge($a1,$a2);
+        echo json_encode($result);
+    }
+    
+    private function generateAdd($range){
+        $firstNum = mt_rand(0,$range);
+        $secondNum = mt_rand(0,$range);
+        $question = strval($firstNum) . "+" . strval($secondNum);
+        $answer = $firstNum + $secondNum; 
+        $result = array($question => $answer);
+
+        return $result;
+    }
+
+    private function generateMinus($range){
+        $firstNum = mt_rand(0,$range);
+        $secondNum = mt_rand(0,$range);
+        $question = strval($firstNum) . "-" . strval($secondNum);
+        $answer = $firstNum - $secondNum; 
+        $result = array($question => $answer);
+
+        return $result;
+    }
+
+    private function generateMultiply($range){
+        $firstNum = mt_rand(0,$range);
+        $secondNum = mt_rand(0,$range);
+        $question = strval($firstNum) . "*" . strval($secondNum);
+        $answer = $firstNum * $secondNum; 
+        $result = array($question => $answer);
+
+        return $result;
+    }
+
+    private function generateDivide($range){
+        $firstNum = mt_rand(0,$range);
+        $secondNum = mt_rand(0,$range);
+        $question = strval($firstNum) . "/" . strval($secondNum);
+        $answer = $firstNum / $secondNum; 
+        $result = array($question => $answer);
+
+        return $result;
+    }
+
 }
---
+++ TeamWork/Application/Home/Controller/TeacherController.class.php
@@ -0,0 +1,39 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class TeacherController extends Controller {
+    public function index(){
+       $this->display('teacher/index');
+    }
+
+    public function checkLogin(){
+        $data['username'] = I('param.username');
+        $data['password'] = I('param.password');
+
+        if(!$data['username'] || !$data['password']){
+            $result['status'] = 'miss info';
+            $result['message'] = '缺少有效信息';
+
+            echo json_encode($result);
+            exit;
+        }
+
+        $Dao = M('user');
+        $r = $Dao->where($data)->find();
+
+        // var_dump($Dao);
+
+        if($r){
+            session('user', $r);
+
+            $result['status'] = 'success';
+            $result['message'] = '登录成功';
+        } else {
+            $result['status'] = 'fail';
+            $result['message'] = '登录失败';
+        }
+
+        echo json_encode($result);
+    }
+}
+
